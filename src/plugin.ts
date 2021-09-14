67d419ce61861aeb481f8c9f1815c004e04380b9
Warsono <warsono16694@gmail.com>
2021-09-14 17:16:36 +0700

    BREAKING CHANGE: renamed useAuth to injectAuth and fix some plugin definition

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,22 +1,24 @@
 import {inject, App} from 'vue';
 import {createAuth} from './composition';
 import merge from 'lodash/merge';
-import {AuthOptions} from '../types/index';
+import {AuthFunction, AuthOptions} from '../types/index';
 import {Store} from 'vuex';
 import {defaultOptions} from './options';
 
 interface UserPlugin<S> {
-  auth?: typeof createAuth;
+  auth?: AuthFunction;
   store: Store<S>;
   options: AuthOptions;
 }
 
-export const useAuth = (injectKey = 'auth') => inject(injectKey);
+export const injectAuth = (injectKey = 'auth'): AuthFunction | undefined =>
+  inject(injectKey);
 
 export const AuthPlugin = {
   install: (app: App, {auth, store, options}: UserPlugin<unknown>) => {
     if (!auth) {
-      auth = createAuth(store, merge(defaultOptions, options));
+      auth = (store, options) =>
+        createAuth(store, merge(defaultOptions, options));
     }
 
     app.config.globalProperties.$auth = auth;
