ff3aa0737ef0d661756afbde15f7bb5fb28eb0fe
Warsono <warsono16694@gmail.com>
2021-11-02 19:59:42 +0700

    refactor: fix types

--- types/module.d.ts
+++ types/module.d.ts
@@ -3,7 +3,6 @@ import {Module} from 'vuex';
 import {AuthState} from '../src/module';
 import {AuthOptions} from './options';
 import {AuthStorage} from './storage';
-export {AuthState} from './src/module';
 
 export function authModule<R>(
   initialState: Record<string, any>,
--- types/storage.d.ts
+++ types/storage.d.ts
@@ -1,4 +1,4 @@
-import {AuthOptions} from './options.d';
+import {AuthOptions, SupportedAuthStorage} from './options.d';
 export interface AuthStorage {
   set(key: string, value: any): void;
   get<T>(key: string, defaultValue?: any): T;
